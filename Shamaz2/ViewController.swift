55303997c76817558a6c643482f8c2bd459b7a5d
PIerre-Henri Bruchon <pierre-henri@bruchon.com>
2019-04-27 17:25:06 +0200

    Initial Commit

---
+++ Shamaz2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shamaz2
+//
+//  Created by Pierre-Henri on 27.04.19.
+//  Copyright © 2019 Pierre-Henri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
